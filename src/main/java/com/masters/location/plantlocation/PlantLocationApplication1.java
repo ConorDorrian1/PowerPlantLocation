b2621ea3844d51e959de34aaadbc795490236b97
ConorDorrian1 <L00101441@student.lyit.ie>
2019-10-07 20:46:52 +0100

    added a eureka client

--- src/main/java/com/masters/location/plantlocation/PlantLocationApplication1.java
+++ src/main/java/com/masters/location/plantlocation/PlantLocationApplication1.java
@@ -2,10 +2,12 @@ package com.masters.location.plantlocation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.masters.location.plantlocation.Controller"}) //From package above, so that controller can see and access this file
+@EnableEurekaClient //Letting this service know it a client of service discovery, making it explicit
 public class PlantLocationApplication1 {
 
 	public static void main(String[] args) {
