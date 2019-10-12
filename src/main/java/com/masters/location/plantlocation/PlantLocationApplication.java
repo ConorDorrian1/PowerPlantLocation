b1fe59a8e55690bf744e7df3bd148ce0c7c733a7
ConorDorrian1 <L00101441@student.lyit.ie>
2019-10-12 17:02:07 +0100

    jkj

--- src/main/java/com/masters/location/plantlocation/PlantLocationApplication.java
+++ src/main/java/com/masters/location/plantlocation/PlantLocationApplication.java
@@ -2,16 +2,17 @@ package com.masters.location.plantlocation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.masters.location.plantlocation.Controller"}) //From package above, so that controller can see and access this file
+//@ComponentScan(basePackages={"com.masters.location.plantlocation.Controller"}) //From package above, so that controller can see and access this file
 @EnableEurekaClient //Letting this service know it a client of service discovery, making it explicit
-public class PlantLocationApplication1 {
+public class PlantLocationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlantLocationApplication1.class, args);
+		SpringApplication.run(PlantLocationApplication.class, args);
 	}
 
 }
